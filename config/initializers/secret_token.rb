9cc8cb870e282523ed5d24f5d0bd5619106ee38c
Template User <template@project2.(none)>
2013-10-23 12:05:29 -0600

    initial commit: Albertas_Historical_Cemeteries_dev

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HistoricalCemeteriesDev::Application.config.secret_key_base = '58915fb35e9f38c5bad0b3993fe840ca5a4d23fe5ac43e7498046a95e6c9574910180406bdcf5573f631762c08d923e2452907ad4fd9ada28a02f4730b0b1690'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HistoricalCemeteriesDev::Application.config.session_store :cookie_store, key: '_historical_cemeteries_dev_session'
