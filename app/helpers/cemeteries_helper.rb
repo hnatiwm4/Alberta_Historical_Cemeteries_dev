48dd89d59ba7c05d114052aec97ccb1cc6415f42
hnatiwm4 <hnatiwm4@mymacewan.ca>
2013-11-28 13:43:11 -0700

    Added some sample lat/long values to the databas eto generate points on the map

--- app/helpers/cemeteries_helper.rb
+++ app/helpers/cemeteries_helper.rb
@@ -1,2 +1,15 @@
 module CemeteriesHelper
+
+def add_markers
+  @cemeteries = Cemetery.all
+  @markers_hash = Gmaps4rails.build_markers(@cemeteries) do |cem, marker|
+    if cem.lat != 0 && cem.long != 0 
+      marker.lat cem.lat
+      marker.lng cem.long
+    end
+  end
+  return @markers_hash
+end
+
+
 end
